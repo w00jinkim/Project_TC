f3e4c6e683a9538ca19d9deec5e1332279131b8f
w00jinkim <willjin1209@gmail.com>
2023-01-19 18:42:14 +0900

    초기세팅

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div className="font-bold text-lg">Hi top</div>
+    </div>
+  )
+}
+
+export default Navigation
